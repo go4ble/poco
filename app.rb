77dc678ce3f2af6ae730e7ed6444fae4ea7ecead
Kyle Boone <kboone@marchex.com>
2015-06-30 20:24:23 +0000

    Move secrets into environment variables

--- app.rb
+++ app.rb
@@ -1,11 +1,8 @@
-#let Bundler handle all requires
+# let Bundler handle all requires
 require 'bundler'
 Bundler.require(:default)
 
-FB_APP_ID = '1465168160462768'
-FB_APP_SECRET = '9920f20cfd4574e64cf61a82b726ccae'
-
-use Rack::Session::Cookie, secret: '96a5699ca5c72572d9ee7ef8458c77e3cd0779fd6ba3ae15612db7221be1fe54070e53d037bab3eb06ac73438429cf2d39c1584754a69b6747025527c35f55bd'
+use Rack::Session::Cookie, secret: ENV['COOKIE_SECRET'] || raise('no COOKIE_SECRET')
 
 get '/' do
   if session['access_token']
@@ -17,7 +14,7 @@ end
 
 get '/login' do
   # generate a new oauth object with your app data and callback url
-  session['oauth'] = Koala::Facebook::OAuth.new(FB_APP_ID, FB_APP_SECRET, "#{request.base_url}/callback")
+  session['oauth'] = Koala::Facebook::OAuth.new(ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], "#{request.base_url}/callback")
   # redirect to facebook to get your code
   redirect session['oauth'].url_for_oauth_code(permissions: 'email')
 end
@@ -28,9 +25,9 @@ get '/logout' do
   redirect '/'
 end
 
-#method to handle the redirect from facebook back to you
+# method to handle the redirect from facebook back to you
 get '/callback' do
-  #get the access token from facebook with your code
+  # get the access token from facebook with your code
   session['access_token'] = session['oauth'].get_access_token(params[:code])
   redirect '/'
 end
