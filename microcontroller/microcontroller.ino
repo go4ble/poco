651baf17fcb2d862510da0ae49e51e614ba1e72d
Kyle Boone <go4ble23@gmail.com>
2015-07-01 22:15:39 -0700

    Add door status query to microcontroller

--- microcontroller/microcontroller.ino
+++ microcontroller/microcontroller.ino
@@ -1,15 +1,20 @@
 #include "limits.h"
 
-#define GET_TEMP    "get_temp"
-#define TOGGLE_DOOR "toggle_door"
+#define GET_TEMP        "get_temp"
+#define TOGGLE_DOOR     "toggle_door"
+#define GET_DOOR_STATUS "get_door_status"
 
-#define TEMP_PIN 0   // analog read
-#define DOOR_PIN 13  // digital write
+#define TEMP_PIN 0         // analog read
+#define DOOR_PIN 13        // digital write
+#define DOOR_OPEN_PIN 7    // digital read
+#define DOOR_CLOSED_PIN 6  // digital read
 
 void setup() {
   Serial.begin(9600);
   pinMode(DOOR_PIN, OUTPUT);
   digitalWrite(DOOR_PIN, LOW);
+  pinMode(DOOR_OPEN_PIN, INPUT_PULLUP);
+  pinMode(DOOR_CLOSED_PIN, INPUT_PULLUP);
 }
 
 #define BUFMAX 32
@@ -20,6 +25,10 @@ int buflen = 0;
 bool doorToggled = false;
 unsigned long doorToggledAt;
 
+#define DOOR_STATUS_OPEN    "open"
+#define DOOR_STATUS_CLOSED  "closed"
+#define DOOR_STATUS_UNKNOWN "unkown"
+
 void loop() {
   while (Serial.available()) {
     char c = Serial.read();
@@ -29,6 +38,8 @@ void loop() {
         getTemp();
       } else if (!strcmp(TOGGLE_DOOR, buf)) {
         startToggleDoor();
+      } else if (!strcmp(GET_DOOR_STATUS, buf)) {
+        getDoorStatus();
       }
       buflen = 0;
     } else {
@@ -62,5 +73,15 @@ void startToggleDoor() {
 void endToggleDoor() {
   doorToggled = false;
   digitalWrite(DOOR_PIN, LOW);
+}
+
+void getDoorStatus() {
+  if (digitalRead(DOOR_OPEN_PIN) == LOW) {
+    Serial.println(DOOR_STATUS_OPEN);
+  } else if (digitalRead(DOOR_CLOSED_PIN) == LOW) {
+    Serial.println(DOOR_STATUS_CLOSED);
+  } else {
+    Serial.println(DOOR_STATUS_UNKNOWN);
+  }
 }
 
